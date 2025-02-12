78d9d3f9c3a1ea6e251b74e8f689bf2739351505
klodska <klodskateam@gmail.com>
2025-02-12 21:22:56 +0300

    вроде готовое управление для мобилок😃😃😃

--- button_shoot.gd
+++ button_shoot.gd
@@ -14,3 +14,7 @@ func _on_button_sprint_toggled(toggled_on: bool) -> void:
 
 func _on_button_reload_pressed() -> void:
 	$"../../../game/player".bullets_reload()
+
+func _ready() -> void:
+	if OS.get_name() != "Android":
+		$"..".queue_free()
