f9a823fc9d034e0615563c89a66bd0b418518c7b
klodska <klodskateam@gmail.com>
2025-02-12 21:03:07 +0300

    готовое управление для мобилки попытка 1

--- button_shoot.gd
+++ button_shoot.gd
@@ -11,3 +11,9 @@ func _on_button_sprint_toggled(toggled_on: bool) -> void:
 		Input.action_press("run")
 	if !toggled_on:
 		Input.action_release("run")
+
+
+func _on_button_reload_button_down() -> void:
+	Input.action_press("reload")
+func _on_button_reload_button_up() -> void:
+	Input.action_release("reload")
