e59f613b5cf0c131057b8d5cfab319eb43993829
klodska <klodskateam@gmail.com>
2025-02-12 21:15:41 +0300

    ааа

--- button_shoot.gd
+++ button_shoot.gd
@@ -12,8 +12,5 @@ func _on_button_sprint_toggled(toggled_on: bool) -> void:
 	if !toggled_on:
 		Input.action_release("run")
 
-
-func _on_button_reload_button_down() -> void:
-	Input.action_press("reload")
-func _on_button_reload_button_up() -> void:
-	Input.action_release("reload")
+func _on_button_reload_pressed() -> void:
+	$"../../../game/player".bullets_reload()
