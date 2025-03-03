9430cc6faf4d795e0e877a0d2d79fac8008932a4
mw10b1909 <martinwin10b1909@gmail.com>
2025-03-03 21:10:09 +0200

    фпс фикс

--- fps.gd
+++ fps.gd
@@ -2,4 +2,4 @@ extends Label
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta: float) -> void:
-	text = str(Engine.get_frames_per_second()) + " FPS"
+	text = str(int(Engine.get_frames_per_second())) + " FPS"
