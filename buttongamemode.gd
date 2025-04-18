b0921884c81a740c04b900b71daf26e646414ebd
mw10b1909 <martinwin10b1909@gmail.com>
2025-04-18 19:11:07 +0300

    Обновление переводов и фиксы

--- buttongamemode.gd
+++ buttongamemode.gd
@@ -6,7 +6,7 @@ var GMNAME = null
 var GMDESC = null
 
 func _on_pressed() -> void:
-	get_tree().change_scene_to_file(SCENE)
+#	get_tree().change_scene_to_file(SCENE)
 	
 	# ну ваще тут можно было это всё в функцию передать но я осознал это только сейчас лол (а переделовать лень😭😭😭)
 	
