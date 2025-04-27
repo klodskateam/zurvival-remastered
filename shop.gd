5048d1e7f3c1b222147829bdf8a7056f4f17d739
mw10b1909 <martinwin10b1909@gmail.com>
2025-04-27 13:46:22 +0300

    теперь можно выйти из магазина

--- shop.gd
+++ shop.gd
@@ -62,3 +62,8 @@ func _ready() -> void:
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta: float) -> void:
 	pass
+
+
+func _on_shopclose_pressed() -> void:
+	get_tree().change_scene_to_file("res://menu.tscn")
+	pass
