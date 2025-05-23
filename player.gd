082426b5f806a9ba768771833714d7638fb3543f
mw10b1909 <martinwin10b1909@gmail.com>
2025-05-24 01:09:14 +0300

    Анимки камеры

--- player.gd
+++ player.gd
@@ -59,38 +59,50 @@ func _physics_process(delta: float):
 	if Input.is_action_pressed("run") and (Input.is_action_pressed("up") or Input.is_action_pressed("down") or Input.is_action_pressed("left") or Input.is_action_pressed("right")) and RUNLOCK != 1:
 		print(SPEED)
 		print(VINOSLIVOST)
-		if (VINOSLIVOST >= 40):	
-			RUN_SPEED = 410
-			$Camera2D.zoom = Vector2(0.98, 0.98)
+		if (VINOSLIVOST >= 40):
+			RUN_SPEED = 410 
+			fov_up()
+		#	$Camera2D.zoom = Vector2(0.98, 0.98)
 		else:
 			RUN_SPEED = 345
-			$Camera2D.zoom = Vector2(0.987, 0.987)
+			fov_half_up()
+		#	$Camera2D.zoom = Vector2(0.987, 0.987)
 		if (VINOSLIVOST >= 0):
 			SPEED = RUN_SPEED
 			VINOSLIVOST -= 19.5 * delta
 		else:
 			SPEED = REGULAR_SPEED
-			$Camera2D.zoom = Vector2(1, 1)
+			fov_down()
+#			$Camera2D.zoom = Vector2(1, 1)
 			RUNLOCK = 1
 	else:
 		SPEED = REGULAR_SPEED
 		if (VINOSLIVOST <= MAX_VINOSLIVOST) and (SPEED != RUN_SPEED):
 			VINOSLIVOST += 6.5 * delta
-			$Camera2D.zoom = Vector2(1, 1)
+			fov_down()
+#			$Camera2D.zoom = Vector2(1, 1)
 	if (VINOSLIVOST <= 35) and !Input.is_action_pressed("run"):
 		RUNLOCK = 1
 	if (VINOSLIVOST >= 35):
 		RUNLOCK = 0
 		
-		
-	
 	if (OS.get_name() != "Android"):
 		look_at(get_global_mouse_position())
 		rotate(PI / 2)
 	if DELAY <= 1:
 		DELAY += 5.3 * delta
 		print(DELAY)
-	
+
+func fov_up():
+	var tween = $Camera2D.create_tween()
+	tween.tween_property($Camera2D, "zoom", Vector2(0.907, 0.907), 0.8)
+func fov_half_up():
+	var tween = $Camera2D.create_tween()
+	tween.tween_property($Camera2D, "zoom", Vector2(0.954, 0.954), 1.3)
+func fov_down():
+	var tween = $Camera2D.create_tween()
+	tween.tween_property($Camera2D, "zoom", Vector2(1, 1), 1.4)	
+		
 	
 func _process(delta: float):
 	if (OS.get_name() == "Android"):
