321fe954ae9c1600163860df169f3e8cb16eadd9
mw10b1909 <martinwin10b1909@gmail.com>
2025-11-13 18:59:49 +0200

    плавный переход до красной виньетки

--- vignette.gd
+++ vignette.gd
@@ -3,11 +3,18 @@ extends Sprite2D
 @onready var anim = $AnimationPlayer
 var lowhealth = false
 
+func tweenthis(thing : bool):
+	if !thing:
+		create_tween().tween_property(self, "modulate:a", 1, 1.2)
+	else:
+		create_tween().tween_property(self, "modulate:a", 0, 1.2)
+	pass
+
 func _process(delta: float) -> void:
 	if (lowhealth != false):
-		show()
+		tweenthis(0)
 		anim.play("lowhealth_anim")
 	else:
 		anim.stop()
-		hide()
+		tweenthis(1)
 		
