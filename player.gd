7570c6ca541cf7b719c6bc403c4f401ad1ca22b2
klodska <klodskateam@gmail.com>
2025-11-19 00:28:04 +0300

    Автобилд, изменение в зимнем режима

--- player.gd
+++ player.gd
@@ -20,6 +20,17 @@ extends CharacterBody2D
 @onready var vignette_red = $"../UI/VignetteRed"
 
 
+const GRASS_STEP_01 = preload("res://Sound/grass_step_01.wav")
+const GRASS_STEP_02 = preload("res://Sound/grass_step_02.wav")
+const GRASS_STEP_03 = preload("res://Sound/grass_step_03.wav")
+const GRASS_STEP_04 = preload("res://Sound/grass_step_04.wav")
+
+const PICKUP_01 = preload("res://Sound/pickup_01.wav")
+const PICKUP_02 = preload("res://Sound/pickup_02.wav")
+const PICKUP_MEDKIT_01 = preload("res://Sound/pickup_medkit_01.wav")
+const PICKUP_MEDKIT_02 = preload("res://Sound/pickup_medkit_02.wav")
+
+
 
 var VINOSLIVOST = 100
 @export var MAX_VINOSLIVOST = 100
@@ -95,39 +106,49 @@ func _physics_process(delta: float):
 	else:
 		pass
 	
+	
+	if Input.is_action_pressed("run") and (Input.is_action_pressed("up") or Input.is_action_pressed("down") or Input.is_action_pressed("left") or Input.is_action_pressed("right")) and RUNLOCK != 1:
+		print(SPEED)
+		print(VINOSLIVOST)
+		if (VINOSLIVOST >= 40):
+			RUN_SPEED = 410 
+			fov_up()
+		#	$Camera2D.zoom = Vector2(0.98, 0.98)
+		else:
+			RUN_SPEED = 345
+			fov_half_up()
+		#	$Camera2D.zoom = Vector2(0.987, 0.987)
+		if (VINOSLIVOST >= 0):
+			SPEED = RUN_SPEED
+			VINOSLIVOST -= 19.5 * delta
+		else:
+			SPEED = REGULAR_SPEED
+			fov_down()
+#					$Camera2D.zoom = Vector2(1, 1)
+			RUNLOCK = 1
+	else:
+		SPEED = REGULAR_SPEED
+		if (VINOSLIVOST <= MAX_VINOSLIVOST) and (SPEED != RUN_SPEED):
+			VINOSLIVOST += 6.5 * delta
+			fov_down()
+#					$Camera2D.zoom = Vector2(1, 1)
+	if (VINOSLIVOST <= 35) and !Input.is_action_pressed("run"):
+		RUNLOCK = 1
+	if (VINOSLIVOST >= 35):
+		RUNLOCK = 0
+		
 	match GamemodeManager.GAMEMODE:
 		1:
 			pass
-		_:
+		2:
 			if Input.is_action_pressed("run") and (Input.is_action_pressed("up") or Input.is_action_pressed("down") or Input.is_action_pressed("left") or Input.is_action_pressed("right")) and RUNLOCK != 1:
-				print(SPEED)
-				print(VINOSLIVOST)
-				if (VINOSLIVOST >= 40):
-					RUN_SPEED = 410 
-					fov_up()
-				#	$Camera2D.zoom = Vector2(0.98, 0.98)
-				else:
-					RUN_SPEED = 345
-					fov_half_up()
-				#	$Camera2D.zoom = Vector2(0.987, 0.987)
-				if (VINOSLIVOST >= 0):
-					SPEED = RUN_SPEED
-					VINOSLIVOST -= 19.5 * delta
-				else:
-					SPEED = REGULAR_SPEED
-					fov_down()
-#					$Camera2D.zoom = Vector2(1, 1)
-					RUNLOCK = 1
+				SPEED = clamp(RUN_SPEED - (INVENTORY_FILLED*5), 150, 400)
 			else:
-				SPEED = REGULAR_SPEED
-				if (VINOSLIVOST <= MAX_VINOSLIVOST) and (SPEED != RUN_SPEED):
-					VINOSLIVOST += 6.5 * delta
-					fov_down()
-#					$Camera2D.zoom = Vector2(1, 1)
-			if (VINOSLIVOST <= 35) and !Input.is_action_pressed("run"):
-				RUNLOCK = 1
-			if (VINOSLIVOST >= 35):
-				RUNLOCK = 0
+				SPEED = clamp(REGULAR_SPEED - (INVENTORY_FILLED*5), 150, 400)
+		_:
+			pass
+	
+	
 		
 	if (OS.get_name() != "Android"):
 		look_at(get_global_mouse_position())
@@ -196,22 +217,22 @@ func _input(event):
 	if event.is_action_pressed("reload"):
 		bullets_reload()
 	if pickedup:
+		$Pickup01.pitch_scale = randf_range(0.97, 1.12)
 		match randi_range(1,2):
 			1:
-				$Pickup01.pitch_scale = randf_range(0.97, 1.12)
-				$Pickup01.play()
+				$Pickup01.stream = PICKUP_01
 			2:
-				$Pickup02.pitch_scale = randf_range(0.97, 1.12)
-				$Pickup02.play()
+				$Pickup01.stream = PICKUP_01
+		$Pickup01.play()
 	pickedup = false
 	if pickedup_medkit:
+		$PickupMedkit01.pitch_scale = randf_range(0.96, 1.12)
 		match randi_range(1,2):
 			1:
-				$PickupMedkit01.pitch_scale = randf_range(0.96, 1.12)
-				$PickupMedkit01.play()
+				$PickupMedkit01.stream = PICKUP_MEDKIT_01
 			2:
-				$PickupMedkit02.pitch_scale = randf_range(0.96, 1.12)
-				$PickupMedkit02.play()
+				$PickupMedkit01.stream = PICKUP_MEDKIT_01
+		$PickupMedkit01.play()
 	pickedup_medkit = false
 		
 
@@ -253,31 +274,27 @@ func bullets_reload():
 func _on_walkdelay_timeout() -> void:
 	if Input.is_action_pressed("run") and (Input.is_action_pressed("up") or Input.is_action_pressed("down") or Input.is_action_pressed("left") or Input.is_action_pressed("right")) and RUNLOCK != 1:
 		$WalkDelay.wait_time = randf_range(0.20,0.24)
+		$GrassStep01.pitch_scale = randf_range(0.96, 1.02)
 		match randi_range(1,4):
 			1:
-				$GrassStep01.pitch_scale = randf_range(0.96, 1.02)
-				$GrassStep01.play()
+				$GrassStep01.stream = GRASS_STEP_01
 			2:
-				$GrassStep02.pitch_scale = randf_range(0.96, 1.02)
-				$GrassStep02.play()
+				$GrassStep01.stream = GRASS_STEP_02
 			3:
-				$GrassStep03.pitch_scale = randf_range(0.94, 1.05)
-				$GrassStep03.play()
+				$GrassStep01.stream = GRASS_STEP_03
 			4:
-				$GrassStep04.pitch_scale = randf_range(0.94, 1.05)
-				$GrassStep04.play()
+				$GrassStep01.stream = GRASS_STEP_04
+		$GrassStep01.play()
 	elif (Input.is_action_pressed("up") or Input.is_action_pressed("down") or Input.is_action_pressed("left") or Input.is_action_pressed("right")):
 		$WalkDelay.wait_time = randf_range(0.24,0.27)
+		$GrassStep01.pitch_scale = randf_range(0.91, 1.06)
 		match randi_range(1,4):
 			1:
-				$GrassStep01.pitch_scale = randf_range(0.91, 1.06)
-				$GrassStep01.play()
+				$GrassStep01.stream = GRASS_STEP_01
 			2:
-				$GrassStep02.pitch_scale = randf_range(0.91, 1.06)
-				$GrassStep02.play()
+				$GrassStep01.stream = GRASS_STEP_02
 			3:
-				$GrassStep03.pitch_scale = randf_range(0.89, 1.06)
-				$GrassStep03.play()
+				$GrassStep01.stream = GRASS_STEP_03
 			4:
-				$GrassStep04.pitch_scale = randf_range(0.89, 1.02)
-				$GrassStep04.play()
+				$GrassStep01.stream = GRASS_STEP_04
+		$GrassStep01.play()
